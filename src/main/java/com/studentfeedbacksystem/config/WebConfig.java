27af27910ab97a4c994d053ff54d3f7c03426a02
Prasanth631 <prasanthgolla29@gmail.com>
2025-02-06 13:16:48 +0530

    v4

--- src/main/java/com/studentfeedbacksystem/config/WebConfig.java
+++ src/main/java/com/studentfeedbacksystem/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://sfeshackthon.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
